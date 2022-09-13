1126afce35ae66507b4d7e815dc044f005777d8f
Nima <nimarahmati8par@gmail.com>
2022-09-13 13:48:24 +0430

    models Updated, JWT token added, Verification and setting user Done, validators written, Signup and Login Done

--- app/routes/auth/auth.routes.js
+++ app/routes/auth/auth.routes.js
@@ -1,11 +1,12 @@
 const { AuthController } = require("../../http/controllers/auth/auth.controller");
 const { ValidationErrorMapper } = require("../../http/middlewares/validationErrorsMapper");
+const { VerifyTokenAndSetUser } = require("../../http/middlewares/VerifyAccessToken");
 const { signupValidator, LoginValidator } = require("../../http/validators/auth/auth.validator");
 
 const authRouter = require("express").Router();
 
 authRouter.get("/signup", signupValidator(), ValidationErrorMapper, AuthController.signup);
-authRouter.get("/login", LoginValidator(), ValidationErrorMapper, AuthController.Login);
+authRouter.get("/login", LoginValidator(), ValidationErrorMapper, VerifyTokenAndSetUser, AuthController.Login);
 authRouter.get("/forgotPassword")
 
 module.exports = {
